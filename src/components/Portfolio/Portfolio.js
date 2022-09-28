84d3a968e7105ab6e5fd4d2a6f8d48a06d05806b
jaolsen7 <jaolsen@berkeley.edu>
2022-09-27 19:07:29 -0700

    portfolio content

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -14,6 +14,7 @@ const data = [
     id: 1,
     image: IMG1,
     title: "Plant Swap Lite",
+    description: "Full stack, mobile-responsive MERN application for plant hobbyists. Users can CRUD their own posts and/or comment on other's found by querying for username or zipcode. This app utilizes Apollo Client and GraphQL to manage data.",
     github: "https://github.com/jaolsen7/plant-swap",
     demo: "https://stark-brushlands-04635.herokuapp.com/"
   },
@@ -21,6 +22,7 @@ const data = [
     id: 2,
     image: IMG2,
     title: "A. Rodriguez Portfolio",
+    description: "React front-end only and mobile-responsive application for my roommate's professional portfolio. The goal was to have a simple, yet aesthetically pleasing page using plain CSS. This was a template for my personal portfolio, which replaces CSS with SCSS to demonstrate what I learned at my internship.",
     github: "https://github.com/jaolsen7/arod-portfolio",
     demo: "https://www.yahoo.com"
   },
@@ -28,6 +30,7 @@ const data = [
     id: 3,
     image: IMG3,
     title: "Text Me Later",
+    description: "An installable, single-page application that meets PWA criteria. Thus, users can type code offline!",
     github: "https://github.com/jaolsen7/text-me-later",
     demo: "https://tranquil-garden-33513.herokuapp.com/"
   }
@@ -47,7 +50,7 @@ const Portfolio = () => {
         modules={[Pagination, Navigation]}
         className="portfolio">
         {
-          data.map(({id, image, title, github, demo}) => {
+          data.map(({id, image, title, description, github, demo}) => {
             return (
               <SwiperSlide key={id} className="mySwiper portfolio__item">
                 <div className="portfolio__item__image">
@@ -55,7 +58,7 @@ const Portfolio = () => {
                 </div>
                 <h3>{title}</h3>
                 <div className="portfolio__item__description">
-                <small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, nihil. Sapiente quisquam alias ad reiciendis aliquam quod, numquam, deleniti voluptatum, doloribus aut ab omnis incidunt itaque ipsa fugit placeat mollitia.</small>
+                <small>{description}</small>
                 </div>
                 <div className="portfolio__item__cta">
                   <a href={github} className="btn" target="_blank" rel="noreferrer">Github</a>
